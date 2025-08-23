658f0a03eeed6ea0394129f28e09e3245e62474e
TsahiP <69470843+TsahiP@users.noreply.github.com>
2025-08-23 23:22:42 +0300

    refactor: update hero section layout and integrate ProfileCard component

--- src/app/_components/hero-section/content.tsx
+++ src/app/_components/hero-section/content.tsx
@@ -3,35 +3,61 @@ import BlurText from "@/components/ui/blur-text";
 import React from "react";
 import { HeroContentProps } from "./hero";
 import SocialLinks from "@/components/social-links";
+import ProfileCard from "@/components/card/portfolio-card";
 
 const HeroContent = ({ user }: HeroContentProps) => {
   const handleAnimationComplete = () => {
     console.log("Animation completed!");
   };
   return (
-    <div>
-      <BlurText
-        onAnimationComplete={handleAnimationComplete}
-        text={`Hello, I'm`}
-        className="text-1xl font-bold"
-      />
-      <BlurText
-        onAnimationComplete={handleAnimationComplete}
-        text={user?.name}
-        className="text-5xl font-bold"
-      />
-
-      <BlurText onAnimationComplete={handleAnimationComplete} text={`Full Stack Developer`} className="text-3xl font-bold text-[#7658ed]" />
-      <div className="mt-4 max-w-lg">
+    <div className=" flex flex-col md:flex-row gap-10 justify-center items-center mt-12 md:mt-0 ">
+      <div className="md:w-1/2">
         <BlurText
           onAnimationComplete={handleAnimationComplete}
-          text={user?.bio}
-          className="text-1xl font-bold leading-relaxed"
-          animateBy="words"
+          text={`Hello, I'm`}
+          className="text-1xl justify-center md:justify-start font-bold"
         />
+        <BlurText
+          onAnimationComplete={handleAnimationComplete}
+          text={user?.name}
+          className="text-5xl justify-center md:justify-start font-bold  "
+        />
+
+        <BlurText
+          onAnimationComplete={handleAnimationComplete}
+          text={`Full Stack Developer`}
+          className="text-3xl justify-center md:justify-start font-bold text-[#7658ed]"
+        />
+        <div className="mt-4 max-w-lg">
+          <BlurText
+            onAnimationComplete={handleAnimationComplete}
+            text={user?.bio}
+            className="text-1xl justify-center md:justify-start font-bold leading-relaxed"
+            animateBy="words"
+          />
+        </div>
+        <div className="flex flex-row gap-4 mt-4 justify-center md:justify-start">
+          <SocialLinks socialLinks={user?.socialLinks} />
+        </div>
       </div>
-      <div className="flex flex-row gap-4 mt-4">
-        <SocialLinks socialLinks={user?.socialLinks} />
+      <div className="md:w-1/3">
+        <ProfileCard
+          name="Tsahi Pahima"
+          title=""
+          handle="tsahipahima"
+          status="Online"
+          contactText="Contact Me"
+          avatarUrl="/avatar.png"
+          miniAvatarUrl="/avatar.jpg"
+          behindGradient=""
+          innerGradient=""
+          showUserInfo={true}
+          enableTilt={true}
+          iconUrl=""
+          grainUrl=""
+          showBehindGradient={true}
+          className=""
+        />
       </div>
     </div>
   );
--- src/app/_components/hero-section/hero.tsx
+++ src/app/_components/hero-section/hero.tsx
@@ -12,25 +12,6 @@ const Hero = ({ user }: HeroContentProps) => {
   return (
     <div className=" w-full h-full flex flex-row items-center justify-center gap-10">
       <HeroContent user={user} />
-      <div>
-        <ProfileCard
-          name="Tsahi Pahima"
-          title=""
-          handle="tsahipahima"
-          status="Online"
-          contactText="Contact Me"
-          avatarUrl="/avatar.png"
-          miniAvatarUrl="/avatar.jpg"
-          behindGradient="radial-gradient(farthest-side circle at var(--pointer-x) var(--pointer-y),hsla(266,100%,90%,var(--card-opacity)) 4%,hsla(266,50%,80%,calc(var(--card-opacity)*0.75)) 10%,hsla(266,25%,70%,calc(var(--card-opacity)*0.5)) 50%,hsla(266,0%,60%,0) 100%)"
-          innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
-          showUserInfo={true}
-          enableTilt={true}
-          iconUrl=""
-          grainUrl=""
-          showBehindGradient={true}
-          className=""
-        />
-      </div>
     </div>
   );
 };
